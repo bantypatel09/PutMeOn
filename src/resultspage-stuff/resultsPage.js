07da7835bbfc3b36ac1aa7c67a77e45ff913094c
bantypatel09 <61667538+bantypatel09@users.noreply.github.com>
2020-06-05 17:27:57 -0400

    yes

--- src/resultspage-stuff/resultsPage.js
+++ src/resultspage-stuff/resultsPage.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const resultsPage = () => {
-
+return(
+    <Jumbotron>
+        <h3>
+            Penis
+        </h3>
+        </Jumbotron>
+)
 }
 
 export default resultsPage; 
