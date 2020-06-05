1a744572bbad9df09e4131c21e7562f88893c6f2
navey <naveenan.r.y@gmail.com>
2020-06-05 18:24:11 -0400

    Added routing to switch between Home and Results tabs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import Navigation from './other_components/Navbar.js';
+
+import {
+  Route,
+  HashRouter
+} from 'react-router-dom';
+
 import HomePage from './hompage-stuff/homePage.js';
 import resultsPage from './resultspage-stuff/resultsPage.js'
+
 import './css/bootstrap.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Navigation />
-      <HomePage />
-    </div>
+    <HashRouter>
+      <div className="App">
+        <Navigation />
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/results' component={resultsPage}/>
+      </div>
+    </HashRouter>
   );
 }
 
--- src/other_components/Navbar.js
+++ src/other_components/Navbar.js
@@ -2,19 +2,23 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
+import {
+  NavLink
+} from "react-router-dom";
+
 const Navigation = () => {
 
 return(
-    <Navbar bg="secondary" variant="dark" expand="lg">
-  <Navbar.Brand href="#home">Put Me On</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#results">Results</Nav.Link>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
+  <Navbar bg="secondary" variant="dark" expand="lg">
+    <Navbar.Brand href="#home">Put Me On</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="mr-auto">
+        <NavLink className='nav-link' exact to='/'>Home</NavLink>
+        <NavLink className='nav-link' to='/results'>Results</NavLink>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
 
 }
