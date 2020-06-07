c3acf0ab2be6534e44c7d811e0d671243c5ea927
bantypatel09 <61667538+bantypatel09@users.noreply.github.com>
2020-06-06 23:59:50 -0400

    Result Page Update1

--- src/resultspage-stuff/resultsPage.js
+++ src/resultspage-stuff/resultsPage.js
@@ -2,13 +2,26 @@ import React from 'react'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 
 const resultsPage = () => {
-return(
-    <Jumbotron>
-        <h3>
-            Penis
-        </h3>
+    
+        const artists = ['Drake', 'Eminem', 'Kanye'];
+    
+    return(
+        <div>
+        <Jumbotron className="text-center">
+            <h3>
+                Linked Artists
+            </h3>
         </Jumbotron>
-)
+        
+        <div>
+            {artists.map(artist => (
+            <Jumbotron>
+                <p>{artist}</p>
+            </Jumbotron>))}
+        </div>
+        </div>
+    );
+
 }
 
 export default resultsPage; 
